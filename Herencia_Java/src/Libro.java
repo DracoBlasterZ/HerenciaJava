5ea6cdec015b4ff8307222fedaaec75c2b315d64
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2023-03-15 10:46:10 +0100

    Libro Con todas las cosas

--- Herencia_Java/src/Libro.java
+++ Herencia_Java/src/Libro.java
@@ -1,7 +1,26 @@
 
 public class Libro {
-	double ISBN;
+	private double ISBN;
+	private String titulo;
+	private String autor;
+	private int numPag;
 	
-	
-
+	public Libro()
+	{
+		this.ISBN=1340901217;
+		this.titulo=null;
+		this.autor=null;
+		this.numPag=30;
+	}
+	public Libro(double ISBN_new)
+	{
+		this.ISBN=ISBN_new;
+	}
+	public Libro(String title,String author,int nuPag)
+	{
+		this.titulo=title;
+		this.autor=author;
+		this.numPag=nuPag;
+	}
 }
+
